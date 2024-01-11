143c4d210a64ad3f3eb14a06f7d3f928eef687bf
cospino097 <cospino097@gmail.com>
2024-01-11 08:50:17 -0500

    primer commit

---
+++ src/services/spacex.ts
@@ -0,0 +1,35 @@
+import { type Doc, type APISpaceXResponsive } from "../types/api";
+
+export const getLaunchBy = async ({id}: { id: string}) => {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`) 
+    
+  
+    const  launch = (await res.json()) as Doc 
+
+    console.log(launch)
+
+    return launch
+  }
+
+
+
+
+
+
+export const getLatestLaunches = async () => {
+
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      Options: {
+        limit: 12,
+      },
+    }),
+  });
+
+  const { docs: launches } = (await res.json()) as APISpaceXResponsive;
+  return launches
+};
